e73805510be0f3ad1889cbaf885729cd74780bf4
Pyunghwa Kim <syneffort@gmail.com>
2021-07-25 10:06:39 +0900

    upload form2

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Dropzone from 'react-dropzone';
-import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import { Typography, Button, Form, message, Input, Icon, Select } from 'antd';
 
 const { Title } = Typography;
 const { TextArea } = Input;
 
+const PrivateOptions = [
+    {value: 0, label: '비공개' },
+    {value: 1, label: '공개' },
+];
+
+const CategoryOptions = [
+    {value: 0, label: '영화 & 애니메이션' },
+    {value: 1, label: '자동차 & 탈것' },
+    {value: 2, label: '음악' },
+    {value: 3, label: '애완동물 & 동물' },
+];
+
 function VideoUploadPage() {
+
+    const [VideoTitle, setVideoTitle] = useState("");
+    const [Description, setDescription] = useState("");
+    const [Private, setPrivate] = useState(0);
+    const [Category, setCategory] = useState("영화 & 애니메이션");
+
+    const onTitleChange = (e) => {
+        setVideoTitle(e.currentTarget.value);
+    };
+    
+    const onDescriptionChange = (e) => {
+        setDescription(e.currentTarget.value);
+    };
+
+    const onPrivateChange = (e) => {
+        setPrivate(e.currentTarget.value);
+    };
+
+    const onCategoryChange = (e) => {
+        setCategory(e.currentTarget.value);
+    }
+
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
             <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
@@ -34,28 +68,34 @@ function VideoUploadPage() {
 
             <br/>
             <br/>
-
-            <Input onChange value/>
+            <label>제목</label>
+            <Input onChange={onTitleChange} value={VideoTitle}/>
 
             <br/>
             <br/>
             
             <label>설명</label>
-            <TextArea onChange value/>
+            <TextArea onChange={onDescriptionChange} value={Description}/>
 
             <br/>
             <br/>
 
-            <select onChange>
-                <option value></option>
-            </select>
+            <label>공개</label>
+            <Select onChange={onPrivateChange}>
+                {PrivateOptions.map((item, index) => {
+                    return <option key={item.index} value={item.label}>{item.label}</option>
+                })}
+            </Select>
 
             <br/>
             <br/>
 
-            <select onChange>
-                <option value></option>
-            </select>
+            <label>카테고리</label>
+            <Select onChange={onCategoryChange}>
+                {CategoryOptions.map((item, index) => {
+                    return <option key={item.index} value={item.label}>{item.label}</option>
+                })}
+            </Select>
 
             <br/>
             <br/>
